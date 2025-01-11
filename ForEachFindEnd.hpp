01dbaa8529c567f67e504934f900799ce9e71eb3
Shriram V <shri314@yahoo.com>
2025-01-12 00:13:57 +0530

    Add ForEachFindEnd.hpp

---
+++ ForEachFindEnd.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <algorithm>
+
+template<class Range1, class Range2, class F>
+constexpr
+void
+ForEachFindEnd (
+    Range1&& Rng,
+    Range2&& SRng,
+    F&& func
+    )
+{
+    auto cend = Rng.cend();
+
+    while (true)
+    {
+        auto pos = std::find_end(
+                    Rng.cbegin(), cend,
+                    SRng.cbegin(), SRng.cend()
+                );
+
+        if (pos == cend)
+        {
+            break;
+        }
+
+        if (!func({pos, cend}))
+        {
+            break;
+        }
+
+        cend = pos;
+    }
+}
